5facb8273977c6927aec2b4b3d4253cbac6bf918
hicarop4 <pirr4lh0wface@gmail.com>
2023-03-10 23:53:55 -0300

    handlechange and input format

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,11 @@ export default function Document() {
     <Html lang="pt-br">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
           rel="stylesheet"
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,43 @@
 import Head from "next/head";
+const axios = require("axios");
 import { useState } from "react";
 import { FaExchangeAlt } from "react-icons/fa";
 
-export default function Home() {
-  const [currency, setCurrency] = useState();
+export default function Home({ currencies: { data } }) {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState(0);
+  const formatInput = (text) => {
+    // THANKS GPT
+    if (text === 0) return;
+
+    // convert commas to dots
+    let formatText = text.replace(/\,/gm, ".");
+    // delete non digits characters except dots
+    formatText = formatText.replace(/[^\d.]/gm, "");
+    // delete consecutive dots, changing it to just ONE dot
+    formatText = formatText.replace(/[.]+/gm, ".");
+    // delete all dots after the first being typed
+    formatText = formatText.replace(/\.+/gm, function (match, index) {
+      return index === formatText.indexOf(".") ? "." : "";
+    });
+
+    return formatText;
+  };
+
+  const handleChange = (e) => {
+    const text = e.target.value;
+    const formatText = formatInput(text);
+    const resultValue = ((1 / data.BRL.value) * Number(formatText)).toFixed(2);
+
+    setInput(formatText);
+    setResult(resultValue);
+  };
+
+  const invertCurrency = () => {
+    const temp = input;
+    setInput(result);
+    setResult(temp);
+  };
 
   return (
     <>
@@ -13,38 +47,57 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="bg-white max-w-xl px-8 py-6 rounded-md shadow-lg">
+      <main className="bg-white min-w-[376px] px-8 py-6 rounded-lg shadow-lg border-highlight border-2">
         <section>
           <h1 className="font-bold">Conversor de moedas</h1>
           <div className="flex gap-3 items-center">
             <input
+              onChange={handleChange}
               className="input-text"
+              value={input}
               type="text"
               name="conversor"
               id="conversor"
               placeholder="ex: USD, BRL, EUR"
+              autoComplete="false"
+              autoCorrect="false"
             />
-            <button>
-              <FaExchangeAlt className="text-primary text-2xl" />
+            <button onClick={invertCurrency}>
+              <FaExchangeAlt className="text-primary hover:text-gray-700 text-2xl transition-colors" />
             </button>
             <input
+              value={result}
               className="input-text"
               type="text"
               name="conversor"
               id="conversor"
-              placeholder="ex: USD, BRL, EUR"
+              readOnly
+              autoComplete="false"
+              autoCorrect="false"
             />
           </div>
         </section>
 
         <section className="mt-4">
-          <h1 className="font-bold">Taxa de câmbio</h1>
-          <div>
-            <article className="chart"></article>
-          </div>
+          <h1 className="font-bold">Câmbio</h1>
+          <article className="space-x-4 text-4xl [&>*]:cursor-pointer">
+            <button title="REAL" className="fi fi-br"></button>
+            <button title="USD" className="fi fi-us"></button>
+            <button title="EURO" className="fi fi-eu"></button>
+          </article>
         </section>
       </main>
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const url = `https://api.currencyapi.com/v3/latest?apikey=${process.env.API_KEY}&currencies=EUR%2CUSD%2CBRL`;
+  const { data: currencies } = await axios.get(url);
+
+  return {
+    props: {
+      currencies,
+    }, // will be passed to the page component as props
+  };
 }
